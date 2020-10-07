844cb98f19dda2108b502b1a5b387f60b56f62fa
Paul Garcia <paulhiram@hotmail.com>
2020-10-06 21:31:12 -0500

    prueba03

--- main.cpp
+++ main.cpp
@@ -2,8 +2,39 @@
 
 using namespace std;
 
-int main() {
-    cout << "Hola Mundo!" << endl; // eliminar esta línea
+template<typename T>
+void printSize(string t)
+{
+  cout << sizeof(T);
+}
 
+int main() {
+  string tipo;
+  cin >> tipo;
+
+  if (tipo == "char")
+  {
+    printSize<char>(tipo);
+  }
+  else if (tipo == "int")
+  {
+    printSize<int>(tipo);
+  }
+  else if (tipo == "short")
+  {
+    printSize<short>(tipo);
+  }  
+  else if (tipo == "float")
+  {
+    printSize<float>(tipo);
+  }  
+  else if (tipo == "double")
+  {
+    printSize<double>(tipo);
+  }
+  else if (tipo == "long")
+  {
+    printSize<long>(tipo);
+  }  
     return 0;
 }
